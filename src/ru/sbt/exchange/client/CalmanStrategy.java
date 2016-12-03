1defa4b4695d84d6336822533adcf3d91d44d401
mrGlebao <zaborovskij.gleb@gmail.com>
2016-12-04 02:29:13 +0300

    Added stub for calman filter

---
+++ src/ru/sbt/exchange/client/CalmanStrategy.java
@@ -0,0 +1,13 @@
+package ru.sbt.exchange.client;
+
+import ru.sbt.exchange.domain.ExchangeEvent;
+
+/**
+ * Created by zabor on 04.12.2016.
+ */
+public class CalmanStrategy implements AlgoStrategy{
+    @Override
+    public void onEvent(ExchangeEvent event, Broker broker) {
+
+    }
+}
